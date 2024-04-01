17507fbc61c2349bd78faaa26ebb6d84723f124f
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-02 04:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const targetElement = document.getElementById('level');
+
+let domLevel = 0;
+
+let parentElement = targetElement;
+while (parentElement !== document.documentElement) {
+    parentElement = parentElement.parentElement;
+    domLevel++;
+}
+
+// alert('The level of the element is:', domLevel);
